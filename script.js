78ffd0df27fcc2294fd210d9f408fc93522cee0f
nnota141 <145835482+nnota141@users.noreply.github.com>
2026-03-10 23:46:45 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const elements = document.querySelectorAll(".fade-up");
 
-  elements.forEach((el, index) => {
-    el.style.animationDelay = `${index * 0.15}s`;
-  });
+const items = document.querySelectorAll(".fade-up");
+
+items.forEach((item,index)=>{
+item.style.animationDelay = (index*0.12)+"s";
+});
+
 });
