d79bad6fba8441880939cf1cf6084fee393997ba
nnota141 <145835482+nnota141@users.noreply.github.com>
2026-03-11 08:07:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-
-const items = document.querySelectorAll(".fade-up");
-
-items.forEach((item,index)=>{
-item.style.animationDelay = (index*0.12)+"s";
-});
-
+    console.log("Portfolio loaded");
 });
